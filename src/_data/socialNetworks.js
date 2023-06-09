98ff10e2e511de186d6c9d474fbb30639c4510a8
Gérald Ventadour <git.s6hk0@aleeas.com>
2023-06-09 21:28:48 +0200

    add social network data

---
+++ src/_data/socialNetworks.js
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+    image: '/images/iconmonstr-facebook-1.svg',
+    text: 'Visiter notre page Facebook',
+    url: 'https://www.facebook.com/vpstudiofwi/',
+  },
+  {
+    image: '/images/iconmonstr-instagram-11.svg',
+    text: 'Visiter notre page Instagram',
+    url: 'https://www.instagram.com/vpstudio_prod/',
+  },
+  {
+    image: '/images/iconmonstr-linkedin-1.svg',
+    text: 'Visiter notre page Linked in',
+    url: 'https://fr.linkedin.com/in/phannuel-velasques-pv',
+  },
+  {
+    image: '/images/iconmonstr-youtube-1.svg',
+    text: 'Visiter notre page Youtube',
+    url: 'https://www.youtube.com/@vpstudioprod',
+  },
+];
