8cc57725ce2538fcd0614e4fd26a9b1f0495a8d0
Gérald Ventadour <git.s6hk0@aleeas.com>
2023-06-13 22:09:13 +0200

    add project steps

---
+++ src/_data/projectSteps.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    name: 'Pré-production',
+    description:
+      'Nous accordons une grande importance à la prédaration de chaque tournage, car nous considérons que les projets sont tous différents. Nous sommes à votre écoute afin de bien vous conseiller pour une bonne réalisation',
+  },
+  {
+    name: 'Prise de vue',
+    description:
+      'À cette étape nos ressources sont mobilisées afin de capter les meilleurs images pour la prochaine étape.',
+  },
+  {
+    name: 'Post-production',
+    description:
+      'C’est à cette étape que tout notre savoir faire transforme les prises du vue en un visuel impactant. Pour cela nous disposons de matériel informatique assez performant qui puisse traiter des vidéos en 4K et des photos en haute définition.',
+  },
+];
