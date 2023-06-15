708ca05ca9940591d33eadbab58563e79eb14249
Gérald Ventadour <git.s6hk0@aleeas.com>
2023-06-15 20:23:57 +0200

    add customers data

---
+++ src/_data/customers.js
@@ -0,0 +1,24 @@
+const baseUrl = 'images';
+
+module.exports = [
+  {
+    image: `${baseUrl}/amalfi.png`,
+    name: 'Le Grand Amalfi Restaurant',
+    url: 'https://legrandamalfirestaurant.fr',
+  },
+  {
+    image: `${baseUrl}/ekole.png`,
+    name: 'Ekole',
+    url: 'https://www.ekole.fr/',
+  },
+  {
+    image: `${baseUrl}/prison-du-bouffay.png`,
+    name: 'La Prison du Buffay',
+    url: 'https://www.laprisondubouffay.fr/',
+  },
+  {
+    image: `${baseUrl}/shab.png`,
+    name: 'Shab',
+    url: 'https://shab.io',
+  },
+];
