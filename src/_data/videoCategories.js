bf007a7d358b720b0cb85b8937daa8f91318fd6f
Gérald VENTADOUR <git.s6hk0@aleeas.com>
2024-01-20 22:44:37 +0100

    publish

--- src/_data/videoCategories.js
+++ src/_data/videoCategories.js
@@ -2,22 +2,30 @@ const baseUrl = '/realisations';
 
 module.exports = [
   {
+    image: 'images/marketing.png',
     name: 'Marketing',
+    slug: 'marketing',
     subcategories: ['Brand content', 'Packshot', 'Publicité'],
     url: `${baseUrl}/marketing`,
   },
   {
+    image: 'images/corporate.png',
     name: 'Corporate',
+    slug: 'corporate',
     subcategories: ['Présentation d’entreprise', 'Témoignage', 'Reportage'],
     url: `${baseUrl}/corporate`,
   },
   {
+    image: 'images/sport.png',
     name: 'Sport',
-    subcategories: ['Training', 'Lifettyle'],
+    slug: 'sport',
+    subcategories: ['Training', 'Lifestyle'],
     url: `${baseUrl}/sport`,
   },
   {
+    image: 'images/after-event.png',
     name: 'After event',
+    slug: 'after-event',
     subcategories: ['Conférence', 'Séminaire', 'Congrès', 'Mariage', 'Salon'],
     url: `${baseUrl}/after-event`,
   },
